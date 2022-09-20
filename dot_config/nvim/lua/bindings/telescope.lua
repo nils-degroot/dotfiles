bb95674cc3b2a27855c724201528156c136a560f
nils-degroot <nils@peeko.nl>
2022-09-20 21:37:47 +0200

    Added vim bindings for branch checkout

--- dot_config/nvim/lua/bindings/telescope.lua
+++ dot_config/nvim/lua/bindings/telescope.lua
@@ -5,3 +5,5 @@ local silent = util.silent
 
 nnoremap("<Leader>pf", ":Telescope fd<CR>", silent)
 nnoremap("<Leader>pb", ":Telescope buffers<CR>", silent)
+
+nnoremap("<Leader>gb", ":Telescope git_branches<CR>", silent)
