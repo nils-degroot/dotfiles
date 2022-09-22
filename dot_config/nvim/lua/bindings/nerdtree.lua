d5d7daa75cff623dc681911ab38725e3f47c6c2a
nils-degroot <nils@peeko.nl>
2022-09-22 07:32:56 +0200

    Cleanup

--- dot_config/nvim/lua/bindings/nerdtree.lua
+++
@@ -1,7 +0,0 @@
-local util = require("bindings.util")
-
-local nnoremap = util.nnoremap
-local silent = util.silent
-
-nnoremap("<Leader>tt", ":NvimTreeToggle<CR>", silent)
-nnoremap("<Leader>pa", ":NvimTreeRefresh<CR>", silent)
--- dot_config/nvim/lua/bindings/telescope.lua
+++
@@ -1,9 +0,0 @@
-local util = require("bindings.util")
-
-local nnoremap = util.nnoremap
-local silent = util.silent
-
-nnoremap("<Leader>pf", ":Telescope fd<CR>", silent)
-nnoremap("<Leader>pb", ":Telescope buffers<CR>", silent)
-
-nnoremap("<Leader>gb", ":Telescope git_branches<CR>", silent)
