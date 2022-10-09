24d467c9523a8dd59ca6c5c39c652bebbe8e1c29
nils-degroot <nils@peeko.nl>
2022-10-09 20:45:17 +0200

    Cleanup

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -12,25 +12,12 @@ return require("packer").startup(function()
 	use("udalov/kotlin-vim")
 	use("purescript-contrib/purescript-vim")
 	use("digitaltoad/vim-pug")
-	use({
-		"evanleck/vim-svelte",
-		branch = "main",
-	})
-	use({
-		"wesleimp/stylua.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-	})
+	use({ "evanleck/vim-svelte", branch = "main" })
+	use({ "wesleimp/stylua.nvim", requires = { "nvim-lua/plenary.nvim" } })
 
 	-- Writing
-	use("junegunn/limelight.vim")
-	use({
-		"folke/twilight.nvim",
-		config = require("plugins.config-twilight"),
-	})
-	use({
-		"folke/zen-mode.nvim",
-		config = require("plugins.config-zen"),
-	})
+	use({ "folke/twilight.nvim", config = require("plugins.config-twilight") })
+	use({ "folke/zen-mode.nvim", config = require("plugins.config-zen") })
 	use("nils-degroot/citatie")
 	use("https://git.sr.ht/~renerocksai/telekasten.nvim")
 	use({
@@ -44,15 +31,11 @@ return require("packer").startup(function()
 	use({ "lewis6991/gitsigns.nvim", config = require("plugins.config-gitsigns") })
 	use({
 		"feline-nvim/feline.nvim",
-		requires = {
-			"kyazdani42/nvim-web-devicons",
-		},
+		requires = { "kyazdani42/nvim-web-devicons" },
 		config = require("plugins.config-feline"),
 	})
 	use("kyazdani42/nvim-tree.lua")
 	use("glepnir/dashboard-nvim")
-	use("nvim-lua/plenary.nvim")
-	use("kyazdani42/nvim-web-devicons")
 	use("folke/which-key.nvim")
 	use({
 		"nvim-telescope/telescope.nvim",
@@ -77,28 +60,20 @@ return require("packer").startup(function()
 			"hrsh7th/cmp-nvim-lua",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
 			"saadparwaiz1/cmp_luasnip",
 		},
+		config = require("plugins.config-cmp"),
 	})
 	use("L3MON4D3/LuaSnip")
-	use({
-		"Saecki/crates.nvim",
-		config = function()
-			require("crates").setup()
-		end,
-	})
+	use({ "Saecki/crates.nvim", config = require("plugins.config-crates") })
 
 	-- etc
 	use("tpope/vim-surround")
-	use("savq/melange")
 	use("mattn/emmet-vim")
 	use("srcery-colors/srcery-vim")
 	use("lewis6991/impatient.nvim")
-	use("editorconfig/editorconfig-vim")
-	use("junegunn/vim-emoji")
 	use({ "phaazon/hop.nvim", config = require("plugins.config-hop") })
-	use({
-		"lukas-reineke/indent-blankline.nvim",
-		config = require("plugins.config-indent-blankline"),
-	})
+	use({ "lukas-reineke/indent-blankline.nvim", config = require("plugins.config-indent-blankline") })
+	use("~/Documents/code/lua/cmp-templates.nvim")
 end)
