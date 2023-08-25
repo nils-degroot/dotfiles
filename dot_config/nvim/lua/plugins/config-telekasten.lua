4f864d5dbfec80aa78d71cc416d08b1a9de97faf
nils-degroot <nils@peeko.nl>
2023-08-25 15:41:01 +0200

    Telekasten templates

--- dot_config/nvim/lua/plugins/config-telekasten.lua
+++ dot_config/nvim/lua/plugins/config-telekasten.lua
@@ -1,9 +1,8 @@
 return function()
-	local home = vim.fn.expand("~/Documents/notes")
-
 	require("telekasten").setup({
-		home = home,
+		home = vim.fn.expand("~/Documents/notes"),
 		tag_notation = "yaml-bare",
-		template_new_daily = home .. "/templates/daily.md",
+		template_new_daily = vim.fn.expand("~/Documents/notes/templates/daily.md"),
+		template_new_note = vim.fn.expand("~/Documents/notes/templates/zettelkasten-note.md"),
 	})
 end
