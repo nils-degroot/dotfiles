31229da0200485c286c4df31b3c74f1f84b70765
nils-degroot <nils@peeko.nl>
2024-07-17 12:48:05 +0200

    Add toggle laptop display binding

--- .config/nvim/lua/custom/plugins/ui.lua
+++ .config/nvim/lua/custom/plugins/ui.lua
@@ -12,12 +12,9 @@ return {
 			wilder.setup({ modes = { ":", "/", "?" } })
 			wilder.set_option(
 				"renderer",
-				wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-					highlights = {
-						border = "Normal", -- highlight to use for the border
-					},
-					border = "rounded",
-				}))
+				wilder.popupmenu_renderer({
+					highlighter = wilder.basic_highlighter(),
+				})
 			)
 		end,
 	},
