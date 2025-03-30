51520f0cbb6b2f548797961bc1c6c90e0690c240
nils-degroot <nils@peeko.nl>
2025-03-30 14:47:45 +0200

    Add a player widget

--- .config/eww/lauch-eww.sh
+++ .config/eww/lauch-eww.sh
@@ -5,7 +5,7 @@ CONFIG_FILES="$HOME/.config/eww/eww.yuck $HOME/.config/eww/eww.scss"
 trap "killall eww" EXIT
 
 while true; do
-    eww open-many uptime system &
+    eww open-many uptime system player &
     inotifywait -e create,modify $CONFIG_FILES
     killall eww
 done
