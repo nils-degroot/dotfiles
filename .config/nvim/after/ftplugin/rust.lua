e207be3a805c7d65fe5ab08cca8fc6acf62a0583
nils-degroot <nils@peeko.nl>
2025-05-15 12:37:55 +0200

    Set discover arg for rustc

--- .config/nvim/after/ftplugin/rust.lua
+++ .config/nvim/after/ftplugin/rust.lua
@@ -5,6 +5,9 @@ vim.g.rustaceanvim = {
 	server = {
 		default_settings = {
 			["rust-analyzer"] = {
+				rustc = {
+					source = "discover",
+				},
 				cargo = {
 					allFeatures = true,
 				},
